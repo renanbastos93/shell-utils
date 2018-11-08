4d746b3c01ff294a7c1f29917d1734933dd0a19c
RENAN.BASTOS <renanbastos.tec@gmail.com>
2018-11-08 11:49:53 -0200

    Update some-alias.sh

--- some-alias.sh
+++ some-alias.sh
@@ -1,4 +1,4 @@
 # Run server Python with Build CSS/ JS using grunt
 alias pyserver='python manage.py runserver_plus 0.0.0.0:8080'
 alias pysass='grunt build_css:styleguide && pyserver'
-alias pyjs='build_js:dev && pyserver'
+alias pyjs='grunt build_js:dev && pyserver'
