3cdfdf39eec23cfddc41774d6b04e8e7a361064c
RENAN.BASTOS <renanbastos.tec@gmail.com>
2018-11-20 16:34:11 -0200

    Update some-alias.sh

--- some-alias.sh
+++ some-alias.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-# Functions to auxiliar alias
-function pyRemoveDataInTable(){
+# Functions to auxiliar alias pySql
+function pySql-removeData(){
     TABLE=$1
     FIELD=$2
     echo -e "use azionmanager; delete from $TABLE where name='$FIELD';" | python manage.py dbshell
 }
 
-function pyLastMigration(){
-    echo -e "use azionmanager; select * from django_migration DESC LIMIT 1;" | python manage.py dbshell
+function pySql-lastMigration(){
+    echo -e "use azionmanager; select * from django_migrations order by name DESC LIMIT 1;" | python manage.py dbshell
 }
 
-function pySelectAllDataInTable(){
+function pySql-listAll(){
     TABLE=$1
     echo -e "use azionmanager; select * from $TABLE;" | python manage.py dbshell
 }
 
-function pySelectLimitDataInTable(){
+function pySql-listLimit(){
     TABLE=$1
     LIMIT=$2
     echo -e "use azionmanager; select * from $TABLE LIMIT $LIMIT;" | python manage.py dbshell
@@ -33,11 +33,6 @@ alias pySql='echo -e "
     pySql-listAll <your table> \n
     pySql-listLimit <your table> <your limit> \n
 "'
-
-alias pySql-lastMigration='pyLastMigration();'
-alias pySql-removeData='pyRemoveDataInTable();'
-alias pySql-listAll='pySelectAllDataInTable();'
-alias pySql-listLimit='pySelectLimitDataInTable();'
 
 # Alias to build Python or Python + (JS or SASS)
 alias pyserver='python manage.py runserver_plus 0.0.0.0:8080'
