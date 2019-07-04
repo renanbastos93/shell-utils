06501fbb7e659cbf00df43cd47dd5bb27ea52277
RENAN.BASTOS <renanbastos.tec@gmail.com>
2019-07-04 19:56:04 -0300

    Update commands-util.sh

--- commands-util.sh
+++ commands-util.sh
@@ -1,3 +1,17 @@
+### Command to remove all branchs by a filter
 git branch | grep "<your search like>" | while read current_branch; \
     do git branch -D $current_branch; \
 done;
+
+# Output steps:
+    ## git branch
+        # dev
+        # master
+        # feat-x
+        # feat-y
+    ## grep "f"
+        # feat-x
+        # feat-y
+    ## while read current_branch; do git branch -D $current_branch; done;
+        # Deleted branch feat-x (was 216e46c).
+        # Deleted branch feat-y (was a6b64c9).
